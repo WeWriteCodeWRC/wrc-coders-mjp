020aa9523d90b5892dec34b144f667f0354564a8
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-10-16 02:58:02 +0530

    card dummy content

--- src/components/ContributorCard.js
+++ src/components/ContributorCard.js
@@ -6,8 +6,20 @@ const Card = () => {
     <div className="container">
       <div className="card">
         <div className="center">
-          <div className="avatar"></div>
+          <div className="avatar">
+            <img
+              className="avatarImage"
+              src="https://avatars.githubusercontent.com/u/69139607?v=4"
+              alt="avatar"
+            />
+          </div>
         </div>
+        <ul className="card-list">
+          <li className="github">
+            <i class="fab fa-github"></i>
+            {"  "} rosekamallove
+          </li>
+        </ul>
       </div>
     </div>
   );
