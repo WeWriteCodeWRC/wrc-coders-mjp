3d42a38dd0a472d4351a3d62d3706721d671574a
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-10-16 00:19:07 +0530

    fetch gh contributors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "../src/styles/main.css";
+import Header from "./components/Header";
 
 function App() {
+  useEffect(() => {
+    const fetchyFetch = async () => {
+      let data = await fetch(
+        "https://api.github.com/repos/rosekamallove/wrc-coders-mjp/contributors"
+      );
+      let realData = await data.json();
+      console.log(realData);
+    };
+    fetchyFetch();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      Hello
     </div>
   );
 }
