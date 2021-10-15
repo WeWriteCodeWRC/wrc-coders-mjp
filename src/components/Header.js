8215c4f3ee42e957815e4c9c3390870524ca1e54
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-10-16 02:05:51 +0530

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/header.css";
+import logo from "./Logo.png";
 
 const Header = () => {
   return (
     <div className="header-main">
       <ul className="nav-ul">
-        <li className="nav-item">Logo</li>
+        <li className="nav-item">
+          <img className="logo" src={logo} alt="Logo"></img>
+        </li>
         <li className="nav-item">
           <a
             className="github-logo"
